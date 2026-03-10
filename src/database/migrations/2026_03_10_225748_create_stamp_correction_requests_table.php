6ee4f583a9429e2a709b046e1c234f8066e49d49
mypchiro <mypchiro@outlook.jp>
2026-03-11 00:16:15 +0900

    migration file add

---
+++ src/database/migrations/2026_03_10_225748_create_stamp_correction_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStampCorrectionRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stamp_correction_requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedTinyInteger('status')->default(0);
+            $table->dateTime('request_start_time');
+            $table->dateTime('request_end_time');
+            $table->varchar('memo');
+            $table->foreignId('attendance_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stamp_correction_requests');
+    }
+}
---
+++ src/database/migrations/2026_03_10_225823_create_rest_stamp_correction_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestStampCorrectionRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rest_stamp_correction_requests', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('request_rest_start');
+            $table->dateTime('request_end_start');
+            $table->foreignId('stamp_correction_request_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rest_stamp_correction_requests');
+    }
+}
